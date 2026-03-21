239557e84ed48ff1b9b33a06949460c3c5c10755
Eric Smrkovsky <33237852+Ericsmrk@users.noreply.github.com>
2026-03-20 23:54:00 -0700

    Initial scaffold: add lesson 1 hello example

---
+++ lessons/01-cpp-foundations/src/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Eric";
+    std::cout << "Hello, " << name << "! Welcome to C++ + SE." << std::endl;
+    return 0;
+}
